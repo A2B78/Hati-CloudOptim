b28ab609b94f4334346c92f074404cf27e6d7133
A2B78 <a2barros78@gmail.com>
2023-10-02 23:23:15 +0200

    Mise a jour des fichiers

--- src/js/app.js
+++ src/js/app.js
@@ -1,83 +1,50 @@
 import '../scss/app.scss';
 
 
-const favoriteButtons = document.querySelectorAll('.favorite-button');
+function loadServer(i) {
+    
+    let template = document.querySelector('#server-template');
 
+    let servers = document.querySelector('.server-list');
 
-const favoriteServers = [];
+    let clone = document.importNode(template.content, true);
 
+    clone.querySelector('.server-name').textContent = `cloud` + i;
 
-function updateFavoriteList() {
-    const serverList = document.querySelector('.server-list');
-
-    serverList.innerHTML = `
-    <span>${server.name}</span>
-    <span>Status: ${server.status}</span>
-    <span>CPU Usage: ${server.cpuUsage}</span>
-    <span>Memory Usage: ${server.memoryUsage}</span>
-`;
-
-    favoriteServers.forEach((server) => {
-        serverList.appendChild(server);
-    });
-}
-
-function handleFavoriteButtonClick(event) {
-    const server = event.target.closest('.server');
-
-    if (server) {
-        const isFavorite = favoriteServers.includes(server);
-
-        if (isFavorite) {
-            favoriteServers.splice(favoriteServers.indexOf(server), 1);
-            event.target.textContent = 'Favoris';
-            event.target.classList.remove('favorited');
-        } else {
-            
-            favoriteServers.push(server);
-            event.target.textContent = 'Favorisé';
-            event.target.classList.add('favorited');
-        }
-
-
-        updateFavoriteList();
+    for (let elm of clone.querySelectorAll('.server')) {
+        const cpuUsage = Math.floor(Math.random() * 100);
+        const memoryUsage = Math.floor(Math.random() * 100);
+        elm.querySelector('.server-cpuUsage').textContent = `CPU utilisé : ${cpuUsage}%`;
+        elm.querySelector('.server-memoryUsage').textContent = `Mémoire utilisée : ${memoryUsage}%`;
     }
-    alert(`${server.querySelector('h2').textContent} a été marqué comme favori!`);
-}
-favoriteButtons.forEach((button) => {
-    button.addEventListener('click', handleFavoriteButtonClick);
-});
-const refreshButton = document.getElementById('refresh-button');
 
-
-function refreshServerStatus() {
-
-    const serverList = document.querySelector('.server-list');
-    serverList.innerHTML = '';
-
-    const serverData = [
-        { name: 'Serveur 1', status: 'En ligne', cpu: '15%', memory: '30%' },
-        { name: 'Serveur 2', status: 'Hors ligne', cpu: '0%', memory: '5%' },
-        { name: 'Serveur 3', status: 'En ligne', cpu: '15%', memory: '20%' },
-        { name: 'Serveur 4', status: 'En ligne', cpu: '15%', memory: '40%' }
-    ];
-
-    serverData.forEach((serverInfo) => {
-        const serverElement = document.createElement('div');
-        serverElement.classList.add('server');
-
-        serverElement.innerHTML = `
-            <h2>${serverInfo.name}</h2>
-            <p>Statut : ${serverInfo.status}</p>
-            <p>CPU utilisé : ${serverInfo.cpu}</p>
-            <p>Mémoire utilisée : ${serverInfo.memory}</p>
-            <button class="favorite-button">Favoris</button>
-        `;
-
-        serverList.appendChild(serverElement);
+    let button = clone.querySelector('.favorite-button');
+    button.addEventListener('click', function () {
+        alert("Le serveur a été ajouté au favoris");
+        button.classList.add('enabled');
     });
 
-    alert('L\'état des serveurs a été actualisé.');
+    servers.appendChild(clone);
 }
 
-refreshButton.addEventListener('click', refreshServerStatus);
+function refreshServerData() {
+    const serverElements = document.querySelectorAll('.server');
+    serverElements.forEach(function (serverElement) {
+        const cpuElement = serverElement.querySelector('.server-cpuUsage');
+        const memoryElement = serverElement.querySelector('.server-memoryUsage');
+
+        const newCPUPercentage = Math.floor(Math.random() * 100);
+        const newMemoryPercentage = Math.floor(Math.random() * 100);
+
+        cpuElement.textContent = `CPU utilisé : ${newCPUPercentage}%`;
+        memoryElement.textContent = `Mémoire utilisée : ${newMemoryPercentage}%`;
+    });
+}
+
+document.getElementById('refresh-button').addEventListener('click', function () {
+    refreshServerData();
+});
+
+for (let i = 0; i < 9; i++) {
+    loadServer(i);
+}
